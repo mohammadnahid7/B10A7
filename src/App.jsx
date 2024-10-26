2911ff00990b21c13fa484296036262dbf1008ac
Mohammad Nahid <62440723+mohammadnahid7@users.noreply.github.com>
2024-10-26 17:46:42 +0900

    Initialized and navbar added

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import "./App.css";
+import Hero from "./components/hero/Hero";
+
+function App() {
+	return <Hero></Hero>;
+}
+
+export default App;
---
+++ src/components/hero/Hero.jsx
@@ -0,0 +1,11 @@
+import Navbar from "./Navbar";
+
+const Hero = () => {
+	return (
+		<>
+			<Navbar></Navbar>
+		</>
+	);
+};
+
+export default Hero;
